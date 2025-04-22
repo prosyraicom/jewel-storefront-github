8846634329674251dbf8c50bf1f6e0d03bcfcbe1
Prosy Arceno <prosy@raicom.co>
2025-04-23 01:12:04 +0800

    set config

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,18 @@ export default {
   experimental: {
     ppr: true,
     inlineCss: true,
-    useCache: true
+    useCache: true,
   },
+  output: "export",
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        pathname: "/s/files/**",
+      },
+    ],
+    unoptimized: true,
+  },
 };
