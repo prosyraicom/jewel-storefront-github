18d0d839221a68ff3d9d8994a3ddef7258266a79
Prosy Arceno <prosy@raicom.co>
2025-04-23 22:35:00 +0800

    add shadcn

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,10 +1,4 @@
-import CartModal from "components/cart/modal";
-import LogoSquare from "components/logo-square";
 import { getMenu } from "lib/shopify";
-import { Menu } from "lib/shopify/types";
-import Link from "next/link";
-import { Suspense } from "react";
-import MobileMenu from "./mobile-menu";
 import Timer from "./timer";
 
 const { SITE_NAME } = process.env;
@@ -58,51 +52,6 @@ export async function Navbar() {
       </div>
 
       {/* White header - menu, logo, cart part */}
-      <nav className="relative flex items-center justify-between p-4 lg:px-6 bg-white border-b">
-        <div className="block flex-none md:hidden">
-          <Suspense fallback={null}>
-            <MobileMenu menu={menu} />
-          </Suspense>
-        </div>
-        <div className="flex w-full items-center">
-          <div className="flex w-full md:w-1/3">
-            <Link
-              href="/"
-              prefetch={true}
-              className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
-            >
-              <LogoSquare />
-              <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
-                {SITE_NAME}
-              </div>
-            </Link>
-            {menu.length ? (
-              <ul className="hidden gap-6 text-sm md:flex md:items-center">
-                {menu.map((item: Menu) => (
-                  <li key={item.title}>
-                    <Link
-                      href={item.path}
-                      prefetch={true}
-                      className="text-neutral-700 underline-offset-4 hover:text-black hover:underline"
-                    >
-                      {item.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            ) : null}
-          </div>
-          <div className="hidden justify-center md:flex md:w-1/3">
-            {/* Image placeholder instead of search */}
-            <div className="w-8 h-8 flex items-center justify-center">
-              <span className="material-symbols-outlined">image</span>
-            </div>
-          </div>
-          <div className="flex justify-end md:w-1/3">
-            <CartModal />
-          </div>
-        </div>
-      </nav>
     </>
   );
 }
